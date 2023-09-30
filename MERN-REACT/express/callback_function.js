0f247a79c30eda6b1035d5067035ce56e62aafad
Shailex13 <128667791+Shailex13@users.noreply.github.com>
2023-09-30 08:52:31 +0000

    callback-function

--- MERN-REACT/express/callback_function.js
+++ MERN-REACT/express/callback_function.js
@@ -4,11 +4,17 @@ function add(x,y){
     console.log(x+y);
 }
 
+function sub(x,y){
+    console.log(x-y);
+}
+
+
 // add(5,6)
-// add(4,5)
+// sub(4,5)
 
 function calc (a,b,op){
     op(a,b);
 }
 
 calc(2,3,add)
+calc(3,1,sub)
